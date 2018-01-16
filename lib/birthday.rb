1209999e99a1fb8d377951bd2aa8a561895e309d
Richard Loera <richexp9@gmx.com>
2018-01-16 02:55:57 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,12 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
+  birthday_kids.each { |e,i|
+    puts "Happy Birthday #{e}! You are now #{i} years old!"
+  }
   # add your code snippet here!
 end
-
-
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,14 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+}
 
 def select_winner(passengers)
+  if passengers[:suite_a].start_with?('A')
+    return passengers[:suite_a]
+  end
   # add the code snippet here!
 end
